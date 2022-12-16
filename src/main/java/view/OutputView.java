07bc1618149f98610209c483163840f88f72c307
Haaae <101785628+Haaae@users.noreply.github.com>
2022-12-16 09:12:29 +0900

    feat : 도메인 기능 구현 이전 구현가능한 출력 기능 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -4,11 +4,58 @@ import domain.Menu;
 import domain.Table;
 
 import java.util.List;
+import view.Enum.MainScreen;
 
 public class OutputView {
     private static final String TOP_LINE = "┌ ─ ┐";
     private static final String TABLE_FORMAT = "| %s |";
     private static final String BOTTOM_LINE = "└ ─ ┘";
+    private static final String MENU_HEADLINE = "## 메인화면";
+    private static final String FUNCTION_SELECTION_REQUEST_HEADLINE = "## 원하는 기능을 선택하세요.";
+    private static final String TABLE_SELECTION_REQUEST_HEADLINE = "## 테이블을 선택하세요.";
+    private static final String MENU_SELECTION_REQUEST_HEADLINE = "## 등록할 메뉴를 선택하세요.";
+    private static final String PAYMENT_SELECTION_REQUEST_HEADLINE = "## 등록할 메뉴를 선택하세요.";
+    private static final String SPACE = " ";
+    private static final String MAIN_SCREEN_BAR = SPACE + "-" + SPACE;
+
+    // 테이블 주문 내역 출력 - domain 구현 후
+    // 특정 테이블 결제 진행 알림 - domain 구현 후
+    // 최종 결제 금액 출력
+
+    // 메인화면 출력
+    public static void printMainScreen() {
+        System.out.println();
+        System.out.println(MENU_HEADLINE);
+
+        for (MainScreen mainScreen : MainScreen.values()) {
+            System.out.println(mainScreen.getSymbol()
+                    + MAIN_SCREEN_BAR
+                    + mainScreen.getName());
+        }
+    }
+
+    // 기능 선택 요청
+    public static void printFunctionSelectionRequest() {
+        System.out.println();
+        System.out.println(FUNCTION_SELECTION_REQUEST_HEADLINE);
+    }
+
+    // 테이블 선택 요청
+    public static void printTableSelectionRequest() {
+        System.out.println();
+        System.out.println(TABLE_SELECTION_REQUEST_HEADLINE);
+    }
+
+    // 등록(주문)할 메뉴 선택 요청
+    public static void printMenuSelectionRequest() {
+        System.out.println();
+        System.out.println(MENU_SELECTION_REQUEST_HEADLINE);
+    }
+
+    // 신용 카드 현금 선택 문구 출력
+    public static void printPaymentSelectionRequest() {
+        System.out.println(PAYMENT_SELECTION_REQUEST_HEADLINE);
+    }
 
     public static void printTables(final List<Table> tables) {
         System.out.println("## 테이블 목록");
